3fcc8bb7882ba67bef0ee0f2044f145e0b355d01
lyes-mersel <dev.mersel@gmail.com>
2025-05-17 03:21:45 +0100

    feat(frontend): implement full reviews listing page

--- app/(store)/product/[productId]/page.tsx
+++ app/(store)/product/[productId]/page.tsx
@@ -65,7 +65,9 @@ export default function ProductPage({
     return (
       <div className="min-h-[calc(100dvh-125px)] flex flex-col items-center justify-center text-center py-12">
         <div className="inline-block h-12 w-12 animate-spin rounded-full border-4 border-solid border-gray-800 border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"></div>
-        <span className="mt-5 text-lg font-medium">Chargement de la page...</span>
+        <span className="mt-5 text-lg font-medium">
+          Chargement de la page...
+        </span>
       </div>
     );
   }
